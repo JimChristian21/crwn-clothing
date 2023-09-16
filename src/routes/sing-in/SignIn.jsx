93415a3e708ac4e31f4960f170e5dc22347e54e3
JimChristian21 <jimchristianedullantes9@gmail.com>
2023-09-16 13:36:28 +0800

    Added states and scss

--- src/routes/sing-in/SignIn.jsx
+++ src/routes/sing-in/SignIn.jsx
@@ -4,9 +4,15 @@ import {
     createUserDocumentFromAuth
 } from '../../utils/firebase/firebase.utils';
 import SignUpForm from '../../components/sign-up-form/SignUpForm';
+import FormInput from '../../components/form-input/FormInput';
+import { useState } from 'react';
+import './sign-in.styles.scss';
 
 
 const SignIn = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const logGoogleUser = async () => {
 
@@ -15,15 +21,36 @@ const SignIn = () => {
         // console.log(response);
     }
 
+    const handleChange = (e) => {
+        
+        // const { name, value } = e.target;
+
+        // setFormFields({
+        //     ...formFields,
+        //     [name]: value
+        // });
+    }
+
     return (
-        <div>
-            <h1>Signin</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google Popup
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign in with Google Popup
-            </button>
+        <div className="sign-in-container">
+            <h2>I already have an account</h2>
+            <span>Sign in with your email and password.</span>
+            <FormInput 
+                label="Email"
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                required
+            />
+            <FormInput 
+                label="Password"
+                type="password"
+                name="email"
+                value={password}
+                onChange={handleChange}
+                required
+            />
 
             <SignUpForm />
         </div>
